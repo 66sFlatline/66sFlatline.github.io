0ef5b6c62d60c0f2d3a1debb09b970399b911d63
66sFlatline <37862054+66sFlatline@users.noreply.github.com>
2018-03-28 10:26:36 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>Ebicheskaya_sila</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="icon" href="favicon.ico" type="image/x-icon" />
+</head>
+<body>
+	<header>за что, как,и нахуя???</header>
+			<nav>
+				<ul>
+					<li>на хуй</li>
+					<li>и в пизду</li>
+				</ul>
+			</nav>
+	<article>
+	<p>ЛЕНИН и ПАРТИЯ, Близнецы братья, кто более для истории ценен? Кто я? Где я? Бляяяяяааааахахахахахахах</p>
+	</article>
+	<img src="pdr.jpg">
+<p>срань адова, блядь, ссука, что посеешь жил бы в сочи? PHZZZ</p>
+</body>
+</html>
